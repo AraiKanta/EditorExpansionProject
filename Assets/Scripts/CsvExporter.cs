c99af8e516cdc857631ab798691126f8a6ba87f6
K_Arai00 <akgame36@gmail.com>
2021-05-27 16:18:57 +0900

    書き出し用のクラス

---
+++ Assets/Scripts/CsvExporter.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> CSVファイル書き出し用ScriptableObjectのクラス </summary>
+[CreateAssetMenu(menuName = "MyScriptable/Create CSV Exporter")]
+public class CsvExporter : ScriptableObject
+{
+	public string fileName;
+}
