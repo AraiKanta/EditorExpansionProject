186679fdf7e38d75385b033c2e34b65d1852b66f
K_Arai00 <akgame36@gmail.com>
2021-05-27 15:32:52 +0900

    このScriptableObjectアセットには読み込み対象のCSVファイルを参照させる　クラスの追加

---
+++ Assets/Scripts/CSVImporter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> このScriptableObjectアセットには読み込み対象のCSVファイルを参照させる </summary>
+[CreateAssetMenu(menuName = "MyScriptable/Create CSV Impoter")]
+public class CSVImporter : ScriptableObject
+{
+    public TextAsset csvFile;
+}
