0f562589b18a49768691c64ca186c1c5c83fd82e
K_Arai00 <akgame36@gmail.com>
2021-08-30 17:47:18 +0900

    アプリケーション終了させるスクリプトの追加

---
+++ Assets/Scripts/ExitApp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+/// <summary> アプリケーションを終了させるクラス </summary>
+public class ExitApp : MonoBehaviour
+{
+    /// <summary>
+    /// 終了ボタン
+    /// </summary>
+    [System.Obsolete]
+    private void OnApplicationQuit()
+    {
+        // 終了処理をキャンセル
+        Application.CancelQuit();
+    }
+
+    /// <summary>
+    /// 終了
+    /// </summary>
+    public void OnExit()
+    {
+        Application.Quit();
+
+        End();
+    }
+
+    //終了
+    [MenuItem("Custom/End")]
+    private static void End()
+    {
+        EditorApplication.isPlaying = false;
+    }
+}
